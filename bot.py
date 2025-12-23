679494f25027cabe61f10b0d92046f138c46a1a4
shblaster <119750994+shblaster@users.noreply.github.com>
2025-12-23 17:53:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ import sys
 from datetime import datetime
 
 from config import API_HASH, APP_ID, LOGGER, TG_BOT_TOKEN, TG_BOT_WORKERS, FORCE_SUB_CHANNEL, CHANNEL_ID, PORT
+
 pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -1002375375523
 class Bot(Client):
